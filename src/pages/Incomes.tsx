b3d4d0d5cb1eb750be3ee8b5d4d44bb5d670037b
mariamdatuka <mariam.datukishvili.7@gmail.com>
2024-01-02 16:31:26 +0400

    style:main input

--- src/pages/Incomes.tsx
+++ src/pages/Incomes.tsx
@@ -1,12 +1,14 @@
 import Layout from "../components/Layouts/Layout"
+import MainInput from "../components/forms/MainInput"
 
 
 const Incomes = () => {
   return (
     <>
      <Layout title='income' mainTitle='Incomes'>
-         <form>
-          
+         <form className='flex flex-col gap-4'>
+          <MainInput type='text' name='income' id='income' placeholder='text'/>
+           
          </form>
          <div>bla</div>
      </Layout>
