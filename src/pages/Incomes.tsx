d0a8d116824239ebadc51da893376754bfe4853f
mariamdatuka <mariam.datukishvili.7@gmail.com>
2024-01-03 17:59:19 +0400

    style:income page

--- src/pages/Incomes.tsx
+++ src/pages/Incomes.tsx
@@ -1,14 +1,28 @@
 import Layout from "../components/Layouts/Layout"
 import MainInput from "../components/forms/MainInput"
-
+import { useForm } from "react-hook-form"
 
 const Incomes = () => {
+  const {register, }=useForm();
   return (
     <>
      <Layout title='income' mainTitle='Incomes'>
          <form className='flex flex-col gap-4'>
-          <MainInput type='text' name='income' id='income' placeholder='text'/>
-           
+          <MainInput type='text' name='salary' id='salary' placeholder='Salary Title'/>
+          <MainInput type='text' name='amount' id='amount' placeholder='Salary Amount'/>
+          <input type='date' id='date' {...register('date')} placeholder='select date' />
+          <select {...register('option')}>
+            <option disabled selected>Select Option</option>
+            <option value='salary'>Salary</option>
+            <option value='freelancing'>Freelancing</option>
+            <option value='investments'>Investments</option>
+            <option value='stocks'>Stocks</option>
+            <option value='bitcins'>Bitcoins</option>
+            <option value='bank transfer'>Bank transfer</option>
+            <option value='other'>other</option>
+          </select>
+          <MainInput id='reference' name='reference' placeholder='add reference' type='text'/>
+          <button>Add Income</button>
          </form>
          <div>bla</div>
      </Layout>
@@ -17,3 +31,5 @@ const Incomes = () => {
 }
 
 export default Incomes
+
+
