dda0d33379555d2097d40548840bee8f64f4cf2a
mariamdatuka <mariam.datukishvili.7@gmail.com>
2024-01-08 23:27:42 +0400

    feat:change chart

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,15 @@
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
 
 const Dashboard = () => {
   const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
   return (
-    <LineChart width={400} height={400} data={data}>
-    <Line type="monotone" dataKey="uv" stroke="#191730" className='bg-red' />
-    </LineChart>
+    <LineChart width={600} height={300} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <CartesianGrid stroke="#ccc" />
+    <XAxis dataKey="name" />
+    <YAxis />
+  </LineChart>
   )
 }
 
