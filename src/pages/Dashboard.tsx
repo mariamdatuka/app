ef4f0f409adbf073fb35448f7bc61959d1b030cd
mariamdatuka <mariam.datukishvili.7@gmail.com>
2024-01-08 23:22:15 +0400

    feat:re-charts for incomes and expenses

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,11 @@
-
+import { LineChart, Line } from 'recharts';
 
 const Dashboard = () => {
+  const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
   return (
-    <div>
-       <h1>hi</h1>
-       <h6>blabala</h6>
-    </div>
+    <LineChart width={400} height={400} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#191730" className='bg-red' />
+    </LineChart>
   )
 }
 
