ff5826e1551f9c5db1d7040138eee23a67eff6df
mariamdatuka <mariam.datukishvili.7@gmail.com>
2024-01-27 22:41:47 +0400

    chore

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,39 +1,8 @@
 const Transactions = () => {
   return (
-    <div>
-      <h1>Transactions history</h1>
-      <div>
-        <p>Date</p>
-        <p>Time</p>
-        <p>Income</p>
-        <p>Expense</p>
-        <p>Expense</p>
-        <p>Expense</p>
-        <p>Expense</p>
-        <p>Expense</p>
-        <div>
-          love life
-        </div>
-        <main>
-          u deserve the best
-        </main>
-        <a>
-          hahaha
-        </a>
-      </div>
-      <div>
-        <p>Date</p>
-        <p>Time</p>
-        <p>Income</p>
-        <p>Expense</p>
-        <p>Expense</p>
-        <p>Expense</p>
-        <p>Expense</p>
-        <p>Expense</p>
-        <div>
-          love life
-        </div>
-    </div>
+ <>
+ <h1>Transactions</h1>
+ </>
   );
 };
 
